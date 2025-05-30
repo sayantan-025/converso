a858d3e07bbffeec28b2834fee7d73a138df2acb
Sayantan Bera <sayantanbera25@gmail.com>
2025-05-30 14:24:53 +0530

    implement companion form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,15 @@
+import CompanionForm from "@/components/companion-form";
 
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
-  )
-}
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className="w-full  flex flex-col gap-4">
+        <h1>Companion Builder</h1>
 
-export default NewCompanion
+        <CompanionForm />
+      </article>
+    </main>
+  );
+};
+
+export default NewCompanion;
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
+      <body className={`${bricolage.variable} antialiased `}>
         <Navbar />
         {children}
       </body>
